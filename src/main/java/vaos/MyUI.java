72644cc6456af14bb5e8aaf127469f60976e1a0f
dobrosi <dobrosi.andras@hmei.hu>
2017-11-16 08:47:34 +0100

    2nd commit

---
+++ src/main/java/vaos/MyUI.java
@@ -0,0 +1,182 @@
+package vaos;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Push;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.MenuBar.MenuItem;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser
+ * window (or tab) or some part of an HTML page where a Vaadin application is
+ * embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is
+ * intended to be overridden to add component to the user interface and
+ * initialize non-component functionality.
+ */
+@Theme("mytheme")
+@Push
+public class MyUI extends UI {
+
+	private TextArea textArea;
+	private OutputStream out;
+	private HorizontalLayout topPanel;
+	private HorizontalLayout appsPanel;
+	private Button clockButton;
+
+	@Override
+	protected void init(VaadinRequest vaadinRequest) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(false);
+		layout.setSpacing(false);
+
+		final VerticalLayout contentLayout = new VerticalLayout();
+
+		clockButton = new Button();
+
+		final MenuBar barmenu = new MenuBar();
+		MenuItem menuItem = barmenu.addItem("Menu", null, null);
+		MenuItem editor = menuItem.addItem("Editor", null, (i) -> {
+			addWindow();
+		});
+
+		appsPanel = new HorizontalLayout();
+		appsPanel.setSpacing(false);
+
+		topPanel = new HorizontalLayout(barmenu, appsPanel, clockButton);
+		topPanel.setExpandRatio(appsPanel, 1.0f);
+		topPanel.setComponentAlignment(clockButton, Alignment.MIDDLE_RIGHT);
+		topPanel.setMargin(false);
+		topPanel.setSpacing(false);
+		topPanel.addStyleName("menuBar");
+		topPanel.setSizeUndefined();
+		topPanel.setWidth(100.0f, Unit.PERCENTAGE);
+
+		final TextField name = new TextField();
+		textArea = new TextArea();
+		name.setCaption("Type your name here:");
+
+		Button button = new Button("Click Me");
+		button.addClickListener(e -> {
+			try {
+				out.write(name.getValue().getBytes());
+				out.write('\n');
+				out.flush();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		});
+		contentLayout.addComponent(name);
+		contentLayout.addComponent(button);
+
+		layout.addComponents(topPanel, contentLayout);
+		layout.setSizeFull();
+		textArea.setSizeFull();
+		textArea.setEnabled(false);
+
+		setContent(layout);
+
+		try {
+			startXterm();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		startTime();
+	}
+
+	private void addWindow() {
+		Window subWindow = new Window("Editor");
+		TextArea t = new TextArea();
+		t.setSizeFull();
+		VerticalLayout subContent = new VerticalLayout(t);
+		subContent.setSizeFull();
+		subWindow.setContent(subContent);
+		Button b = new Button(subWindow.getCaption());
+		topPanel.addComponent(b);
+		b.addClickListener(l -> {
+			subWindow.focus();
+		});
+		appsPanel.addComponent(b);
+
+		subWindow.setPosition(200, 200);
+		subWindow.setWidth(750, Unit.PIXELS);
+		subWindow.setHeight(250, Unit.PIXELS);
+		subWindow.addCloseListener(l -> {
+			appsPanel.removeComponent(b);
+		});
+		getUI().addWindow(subWindow);
+		subWindow.focus();
+	}
+
+	private void startTime() {
+		Timer timer = new Timer();
+		// task will be scheduled after 5 sec delay
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				access(() -> {
+					clockButton.setCaption(new SimpleDateFormat("HH:mm:ss").format(new Date()));
+					startTime();
+				});
+			}
+		}, 1000);
+	}
+
+	private void startXterm() throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		Process proc = rt.exec("bash");
+
+		InputStream in = proc.getInputStream();
+		out = proc.getOutputStream();
+		InputStream err = proc.getErrorStream();
+
+		startReadThread(in);
+		startReadThread(err);
+	}
+
+	private Runnable startReadThread(InputStream in) {
+		Runnable task = () -> {
+			try {
+				while (true) {
+					int ch = in.read();
+					if (ch > 0) {
+						access(() -> {
+							textArea.setValue(textArea.getValue() + (char) ch);
+						});
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		};
+		new Thread(task).start();
+		return task;
+	}
+
+	@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+	@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+	public static class MyUIServlet extends VaadinServlet {
+	}
+}
